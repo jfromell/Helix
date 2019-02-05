881d9dddc0af270a66ae6b1a2347a3b13cc667e1
Jonas Fromell <jonas.per.fromell@gmail.com>
2019-02-05 21:26:10 +0100

    Begin LibAura event dispatching

--- Helix.lua
+++ Helix.lua
@@ -1,36 +1,9 @@
+local addon, ns = ...
+
+-- Setup AddOn
 Helix = LibStub("AceAddon-3.0"):NewAddon("Helix")
 
---[[
-    {
-        player = {
-            spec = {
-                id = 269,
-                talents = {
-                    [154678] = true,
-                    ...
-                },
-            },
-            buffs = {
-                [154678] = {
-                    up = true,
-                    down = false,
-                    count = 3
-                }
-            },
-            spells = {
-                [154678] = {
-                    cooldown = {
-                        remaining = 3
-                    }
-                }
-            }
-        },
-    }
-]]
+-- Initialize AddOn
+function Helix:OnEnable()
 
---[[
-    Schedule a timer that runs every ? ms.
-]]--
---[[
-    Run the APL every tick of the timer
-]]--
+end
--- Libs/LibAura-1.0/LibAura-1.0.lua
+++ Libs/LibAura-1.0/LibAura-1.0.lua
@@ -1,37 +1,50 @@
 local Lib, oldminor = LibStub:NewLibrary("LibAura-1.0", 10)
 
-Lib.Cache = Lib.Cache or {}
---[[
-    {
-        [spellId] = {
-            
-        }
-    }
-]]--
+-- WoW APIs
+local CreateFrame = CreateFrame
+local CombatLogGetCurrentEventInfo = CombatLogGetCurrentEventInfo
 
+-- Aura Cache
+Lib.Auras = Lib.Auras or {}
+
+-- Event Frame
 Lib.EventFrame = Lib.EventFrame or CreateFrame("Frame", "LibAura10Frame")
 
-local EventFrame = Lib.EventFrame
+-- Event Handlers
+function Handle_SpellAuraApplied(sourceGUID, destGUID, ...)
+    local spellId, spellName, _, auraType, amount = select(12, ...)
+
+    local aura = {
+        name = spellName,
+        source = sourceGUID,
+        destination = destGUID,
+        type = auraType,
+        stacks = amount,
+    }
+
+    -- DEBUG
+    DEFAULT_CHAT_FRAME:AddMessage(spellName .. " was applied!")
+
+    Lib.Auras[spellId] = aura
+end
+
+function Handle_SpellAuraRemoved(sourceGUID, destGUID, ...)
+    local spellId = select(12, ...)
+
+    -- DEBUG
+    DEFAULT_CHAT_FRAME:AddMessage(spellId .. " was removed")
+
+    Lib.Auras[spellId] = nil
+end
 
 -- Scripts
 function EventFrame_OnCombatLogEvent(...)
-    local source = select(5, ...)
+    local _, msg, _, sourceGUID, _, _, _, destGUID, _, _, _ = select(1, ...)
 
-    -- Filter events not originating from the player 
-    if source == "player" then
-        local msg = select(2, ...)
-
-        if msg == "SPELL_AURA_APPLIED" then
-            local type = select(13, ...)
-
-            if type == "BUFF" then
-                local spell = select(10, ...)
-
-                if not Lib.Cache[spell] then
-                    Lib.Cache[spell] = {}
-                end
-            end
-        end
+    if msg == "SPELL_AURA_APPLIED" then
+        Handle_SpellAuraApplied(sourceGUID, destGUID, ...)
+    elseif msg == "SPELL_AURA_REMOVED" then
+        Handle_SpellAuraRemoved(sourceGUID, destGUID, ...)
     end
 end
 
@@ -43,10 +56,26 @@ function EventFrame_OnUnitAuraEvent(unit)
     end
 end
 
+-- WoW Events
+local EventFrame = Lib.EventFrame
+
 EventFrame:SetScript("OnEvent", function(self, event, ...)
     if event == "COMBAT_LOG_EVENT_UNFILTERED" then
         EventFrame_OnCombatLogEvent(CombatLogGetCurrentEventInfo())        
-    else if event == "UNIT_AURA" then
+    elseif event == "UNIT_AURA" then
         EventFrame_OnUnitAuraEvent(...)
     end
 end)
+
+-- LibAura Events
+Lib.Registry = { events = setmetatable({}, {
+    __index = function(t, k) t[k] = {} return t[k] end
+})}
+
+local events = Lib.Registry.events
+
+function Lib.Registry:Fire(event, ...)
+    if not rawget(events, event) or not next(events[event]) then return end
+
+    -- Safely dispatch event with xpcall
+end
